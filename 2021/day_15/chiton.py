016b872e962458a50dca7ecd29da0b0c532e2541
Denis Kovalev <aikikode@gmail.com>
2021-12-27 23:39:01 +0300

    Add day 15 part 2

--- 2021/day_15/chiton.py
+++ 2021/day_15/chiton.py
@@ -36,12 +36,36 @@ def get_shortest_path_len(data: List[List[int]]) -> int:
     return int(distance[-1][-1])
 
 
+def extend_data(data: List[List[int]]) -> List[List[int]]:
+    def increase(val: int) -> int:
+        return 1 if val >= 9 else val + 1
+
+    old_limit = len(data)
+    new_limit = 5 * old_limit
+    extended_data = [[0] * new_limit for _ in range(new_limit)]
+    for i in range(new_limit):
+        for j in range(new_limit):
+            if i < old_limit and j < old_limit:
+                extended_data[i][j] = data[i][j]
+                continue
+            if j >= old_limit:
+                extended_data[i][j] = increase(extended_data[i][j - old_limit])
+            else:
+                extended_data[i][j] = increase(extended_data[i - old_limit][j])
+    return extended_data
+
+
+def get_shortest_extended_path_len(data: List[List[int]]) -> int:
+    return get_shortest_path_len(extend_data(data))
+
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         input_file = sys.argv[1]
         with open(input_file) as fin:
             data = [line.strip() for line in fin]
             print("part 1:", get_shortest_path_len(convert_input(data)))
+            print("part 2:", get_shortest_extended_path_len(convert_input(data)))
 
 assert get_shortest_path_len(convert_input("""1163751742
 1381373672
@@ -53,3 +77,14 @@ assert get_shortest_path_len(convert_input("""1163751742
 3125421639
 1293138521
 2311944581""".split("\n"))) == 40
+
+assert get_shortest_extended_path_len(convert_input("""1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581""".split("\n"))) == 315
